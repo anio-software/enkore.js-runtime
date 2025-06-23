367fa5cefe3f44724375aaca38fb963396b2aed5
eXory2024 <exory@anio.software>
2025-06-23 20:24:28 +0200

    Make sure __internalDoNotUse is set when using defineContextOptions.

--- project/export/v0/defineContextOptions.ts
+++ project/export/v0/defineContextOptions.ts
@@ -1,3 +1,22 @@
-export {
-	createJSRuntimeContextOptions as defineContextOptions
+import type {
+	EnkoreJSRuntimeContextOptions,
+	RawType
+} from "@anio-software/enkore-private.spec"
+
+import {
+	createJSRuntimeContextOptions
 } from "@anio-software/enkore-private.spec/factory"
+
+export function defineContextOptions(
+	options: RawType<EnkoreJSRuntimeContextOptions>
+): EnkoreJSRuntimeContextOptions {
+	const entity = createJSRuntimeContextOptions(options)
+
+	entity.__internalDoNotUse = {
+		originatingPackage: {
+			...options.project.packageJSON
+		}
+	}
+
+	return entity
+}
