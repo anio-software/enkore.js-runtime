11661c94f0afaa1b05c59b7ac8d644083fcf062f
eXory2024 <exory@anio.software>
2025-06-23 19:11:33 +0200

    Remove createContext from v0 API.

--- project/export/v0/createContext.ts
+++
@@ -1,8 +0,0 @@
-import type {RuntimeAPI} from "#~src/v0/RuntimeAPI.ts"
-import {createRuntimeContext} from "#~src/v0/createRuntimeContext.ts"
-
-export const createContext: RuntimeAPI["createContext"] = function(
-	project, options
-) {
-	return createRuntimeContext(project, options)
-}
